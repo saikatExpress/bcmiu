61299b3f16e1dbb7d02cd999e8063abefdfbd01f
Saikat Talukder <saikathosen444@gmail.com>
2024-09-09 18:29:21 +0600

    working on project

--- resources/views/website/layout/app.blade.php
+++ resources/views/website/layout/app.blade.php
@@ -8,7 +8,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>ONNOTOMO | Portfolio management System</title>
+        <title>BCMIU | Bangladesh Capital Market Investor Union</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
