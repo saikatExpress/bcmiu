4716db69a77de663b3cfd574fcd4cb70d4fcb7ab
Saikat Talukder <saikathosen444@gmail.com>
2024-10-25 02:40:26 +0600

    working on admin panel

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                     <i class="fa fa-chart-line fa-3x text-primary"></i>
                     <div class="ms-3">
-                        <p class="mb-2">Today Users</p>
+                        <p class="mb-2">Total Users</p>
                         <h6 class="mb-0">{{ $totalUsers ?? 0 }}</h6>
                     </div>
                 </div>
